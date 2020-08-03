86810f2dac2905fefd5c8ab18858a9bfe238547f
Warren Bain <warren@thoughtcroft.com>
2020-08-03 20:12:59 +1000

    Adding documentation and some minor improvements

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # aquamon
-Aquaponics monitoring using Arduino managed sensors
+
+Aquaponics monitoring using Arduino managed sensors.
+
+I have an aquaponics setup with a 1,000 litre fish tank, 700 litre sump
+tank and 3 x 450 litre media beds made from IBCs in a chop and flip
+style. This system is a CHIFT-PIST system (constant height in fish tank,
+pump in sump tank) and I wanted to automate monitoring of key elements
+governing water quality in the system.
+
+This project uses two different micro-controllers:
+
+* Arduino Uno to collect data from various sensors
+* D1 mini ESP8266 to connect to ThingsBoard over WiFi
+
+There are several components that work together.
+
+## components
+
+### ThingsBoard IoT Platform
+
+I am using ThingsBoard Community edition software for managing IoT services
+including device management, rules based actions, dashboards etc. See
+https://thingsboard.io/ for more information.
+
+There is a comprehensive rules engine for managing events such as device
+off-line, data range violations etc. I am using a Telegram bot to alert
+me to such events on my phone.
+
+### Network connectivity
+
+The D1 Mini ESP8266 controller board is responsible for connecting to
+the ThingsBoard server via my home WiFi network. The
+ThingsBoard_WiFi_Interface Sketch is basically a loop that connects to
+WiFi, connects to the ThingsBoard server and sends a JSON request over
+the Serial connection then waits for data to send on to ThingsBoard.
+
+### Sensor data collector
+
+The Arduino Uno controller board is responsible for collecting data from
+the different sensors and sending them to the D1 Mini over the Serial
+connection whenever data has been requested. The
+ThingsBoard_Sensor_Interface Sketch is a loop that listens to the Serial
+connection for a request and then reads the different sensors and
+forwards the data using JSON in a format that ThingsBoard understands.
+
+## acknowledgements
+
+I am indebted to the internet community for so much good content on how
+to set up and develop code using the Arduino IDE.
+
+The two systems use a serial connection to communicate using an approach
+that I borrowed from Acrobotic (see https://youtu.be/6-RXqFS_UtU).
+
+The different sensors have been interfaced using ideas from:
+
+* [OneWire
+  based](https://lastminuteengineers.com/ds18b20-arduino-tutorial/)  temperature sensors setup
+* [two point
+  calibration](https://www.instructables.com/id/Calibration-of-DS18B20-Sensor-With-Arduino-UNO/) of the temperature sensors
+* using the [water flow
+  sensor](https://www.instructables.com/id/How-to-Use-Water-Flow-Sensor-Arduino-Tutorial/)
+
+In each case though, I have attempted to simplify and improve the
+suggested approaches. Feel free to comment or suggest additional
+improvments or borrow this approach to suit your own situation.
--- ThingsBoard_Sensor_Interface/ThingsBoard_Sensor_Interface.ino
+++ ThingsBoard_Sensor_Interface/ThingsBoard_Sensor_Interface.ino
@@ -1,13 +1,17 @@
 /*------------------------------------------------------------------------------
   21/07/2020
   Author: bainsworld
-  Platforms: ESP8266
+  Platform: Arduino Uno
   Language: C++/Arduino
   File: ThingsBoard_Sensor_Interface.ino
   ------------------------------------------------------------------------------
   Description:
   Controller for connecting to various sensors using the Arduino Uno and then
-  sending the telemetry data to ThingsBoard via an ESP8266 D1 Mini via Serial
+  sending the telemetry data to ThingsBoard via an ESP8266 D1 Mini by Serial
+  communications. Sensors are:
+   - DS18B20 water temperature
+   - DS18B20 air temperature
+   - YF-S201 water fow rate
   ------------------------------------------------------------------------------*/
 
 #include "BainsworldConfig.h"  // sensitive config values from here - goes first
@@ -18,27 +22,28 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Temperature sensors
-// specific hardware address for each sensor
+// temperature sensors - specific hardware address for each sensor
 #define ONE_WIRE_BUS 2
 #define TEMP_RESOLUTION 10
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DeviceAddress waterThermometer = { 0x28, 0x3F, 0x80, 0x90, 0x3A, 0x19, 0x01, 0x49 };
-DeviceAddress airThermometer = { 0x28, 0x1A, 0x52, 0x94, 0x97, 0x08, 0x03, 0xB5 };
+DeviceAddress airThermometer =   { 0x28, 0x1A, 0x52, 0x94, 0x97, 0x08, 0x03, 0xB5 };
 // calibration data { low, high }
 float waterTempRange[] = { 0.5, 100.0 };
-float airTempRange[] = { 1.4, 99.0 };
-float refTempRange[] = { 0.0, 99.6 };
+float airTempRange[] =   { 1.4, 99.0  };
+float refTempRange[] =   { 0.0, 99.6  };
 
-// Messages via UART
+// Flow rate sensor
+#define PULSES_PER_LITRE 450   // From the datasheet (7.5 Hz at 1L/min)
+int flowPin = 3;               // This is the input pin on the Arduino
+volatile int flowCounter;      // This integer needs to be set as volatile to ensure it updates correctly during the interrupt process
+
+// Messages are communicated via UART
 String message = "";
 bool messageReady = false;
 DynamicJsonDocument doc(1024); // ArduinoJson version 6+
 
-// Flow rate sensor
-int flowPin = 3;               // This is the input pin on the Arduino
-volatile int flowCounter;      // This integer needs to be set as volatile to ensure it updates correctly during the interrupt process
 
 void setup() {
   Serial.begin(BW_SERIAL_BAUD);
@@ -59,8 +64,9 @@ void setup() {
   // setup various sensors
   setupThermometer(waterThermometer, "water temperature sensor");
   setupThermometer(airThermometer, "air temperature sensor");
-  pinMode(flowPin, INPUT);           // Sets the flow meter pin as an input;
+  pinMode(flowPin, INPUT);
 }
+
 
 void loop() {
   // Monitor serial communication
@@ -68,7 +74,7 @@ void loop() {
     message = Serial.readString();
     messageReady = true;
   }
-  // Only process message if there's one
+  // Only process message if one is requested
   if (messageReady) {
     // The only messages we'll parse will be formatted in JSON
     if (checkMessage()) {
@@ -84,6 +90,7 @@ void loop() {
     messageReady = false;
   }
 }
+
 
 bool checkMessage() {
   // Attempt to deserialize the JSON-formatted message
@@ -124,20 +131,20 @@ float getTemperature(DeviceAddress deviceAddress, float rawTempRange[]) {
 }
 
 void flashLED() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on
-  delay(BW_RETRY_DELAY);             // wait a bit
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off
+  // turn the LED on and off so we know it is working
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(BW_RETRY_DELAY);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 float calculateFlowRate() {
+  // pulses in one second converted to litres per minute
   float flowCounter = getFlowCounter(1000);
-  float flowRate = flowCounter * 2.25;    // Millilitres per tick (approx)
-  flowRate = flowRate * 60;               // Convert into millilitres / minute
-  flowRate = flowRate / 1000;             // Convert into litres / minute
-  return flowRate;
+  return ((flowCounter / PULSES_PER_LITRE) * 60);
 }
 
 float getFlowCounter(int ticks) {
+  // enable interrupts to count pulses during a defined period
   static int interrupt = digitalPinToInterrupt(flowPin);
   attachInterrupt(interrupt, updateFlowCounter, RISING);
   flowCounter = 0;                  // reset the counter
@@ -146,6 +153,7 @@ float getFlowCounter(int ticks) {
   return flowCounter;
 }
 
-void updateFlowCounter() {     // Interrupt Service Routine
-   flowCounter++;              // Every time this function is called, increment counter by 1
+void updateFlowCounter() {
+  // Interrupt Service Routine to count pulses
+   flowCounter++;
 }
